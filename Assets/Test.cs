dbd0efe840735678a1f0d4d90af532fc12b72d2c
Aresbla238 <tul33016@takaoka.ac.jp>
2023-12-25 17:11:09 +0900

    最初のコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        //配列の各要素に値を代入する
+        points[0] = 55;
+        points[1] = 67;
+        points[2] = 83;
+        points[3] = 25;
+        points[4] = 71;
+
+        //5回処理を繰り返す
+        for (int i = 0; i < 5; i++) ;
+        for (int i = 4; i < 0; i++) ;
+
+
+        
+                
+           
+        
+        
+
+        
+
+
+
+
+
+
+            
+            
+    }
+    
+    
+           
+          
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Update is called once per frame
+
+
+    
+
+    
+
